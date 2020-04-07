275d5b253e801e558d4b7c9fd5f800efb5392fb7
jontran1 <jtran001@citymail.cuny.edu>
2020-04-06 20:15:52 -0400

    Able to reset grid and create a completely new grid with new cols and row values

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const container = document.querySelector('#gridContainer');
-
-function createGrid(n, m){
+/**
+ * Pass in a div container and it creates a div inside a div
+ * n * m divs
+ * @param {div} mainDiv 
+ * @param {*} n 
+ * @param {*} m 
+ */
+function createGrid(mainDiv, n, m){
     let auto = ""
     for(let i = 0; i < n; i++){
         auto += "auto ";
     }
-    container.style.gridTemplateColumns = auto;
+    mainDiv.style.gridTemplateColumns = auto;
 
     for(let i = 0; i < n; i++){
         var newOuterDiv = document.createElement("div");
@@ -16,16 +21,43 @@ function createGrid(n, m){
             newOuterDiv.appendChild(newInnerDiv);
           
         }
-        container.appendChild(newOuterDiv);
+        mainDiv.appendChild(newOuterDiv);
     }
 }
 
 function hover(event){
     console.log(event.target);
     var cell = event.target;
-    // if(cell.className == "white-square"){
-    //     cell.className = "black-square";
-    // }
-    cell.className = "white-square";
+    if(cell.className == "black-square"){
+        cell.className = "white-square";
+    }
 }
-createGrid(16,16);
+
+function destoryGrid(){
+    var childNodes = container.childNodes;
+    console.log(childNodes);
+    for(let i = childNodes.length-1; i >= 0; i--){
+        container.removeChild(childNodes[i]);
+    }
+}
+
+function resetGrid(){
+    var cells = container.getElementsByClassName("white-square");
+    console.log(cells.length)
+    for(let i = cells.length-1 ; i >= 0; i--){
+        cells[i].className = "black-square";
+    }
+    var n = prompt("Please enter new column number", "16");
+    var m = prompt("Please enter new row number", "16");
+    destoryGrid();
+    createGrid(container, n, m);
+
+}
+
+const container = document.querySelector('#gridContainer');
+var n = 5;
+var m = 5;
+createGrid(container, n,m);
+
+const resetButton = document.querySelector('#resetButton');
+resetButton.addEventListener("click", resetGrid);
